762907b88975a22a52b56061e0715b5355b186e5
Karl George <karlgeorge87@gmail.com>
2025-02-19 11:23:08 +0100

    Add nav styling on scroll

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,20 +1,38 @@
 import clsx from 'clsx';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link as LinkScroll } from 'react-scroll';
 
 const NavLink = ({ title }) => (
-  <LinkScroll className='base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5'>
+  <LinkScroll className='uppercase transition-colors duration-500 cursor-pointer base-bold text-p4 hover:text-p1 max-lg:my-4 max-lg:h5'>
     {title}
   </LinkScroll>
 );
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [hasScrolled, setHasScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 32);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-    <header className='fixed top-0 left-0 z-50 w-full py-10'>
-      <div className='container flex h-14 items-center max-lg:px-5'>
-        <a href='' className='lg:hidden flex-1 cursor-pointer z-2'>
+    <header
+      className={clsx(
+        'fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4',
+        hasScrolled && 'py-2 bg-black-100 backdrop-blur-[8px]'
+      )}
+    >
+      <div className='container flex items-center h-14 max-lg:px-5'>
+        <a href='' className='flex-1 cursor-pointer lg:hidden z-2'>
           <img src='/images/xora.svg' alt='logo' width={115} height={55} />
         </a>
 
@@ -80,13 +98,13 @@ const Header = () => {
 
         {/* Hamburger button */}
         <button
-          className='lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center'
+          className='flex items-center justify-center border-2 rounded-full lg:hidden z-2 size-10 border-s4/25'
           onClick={() => setIsOpen((prevState) => !prevState)}
         >
           <img
             src={`/images/${isOpen ? 'close.svg' : 'magic.svg'}`}
             alt='button'
-            className='size-1/2 object-contain'
+            className='object-contain size-1/2'
           />
         </button>
       </div>
